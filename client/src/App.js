17411c15336b4c064c391cbb1082103d5f49a955
loumonthjack <53491428+loumonthjack@users.noreply.github.com>
2020-01-27 00:50:23 -0600

    Add files via upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,64 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
 
-import CreateEmployee from './components/CreateEmployee';
-import ShowEmployeeList from './components/ShowEmployeeList';
-import ShowEmployeeDetails from './components/ShowEmployeeDetails';
-import UpdateEmployeeInfo from './components/UpdateEmployeeInfo';
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { Provider } from "react-redux";
+import store from "./store";
 
+import Navbar from "./components/layout/Navbar";
+import Landing from "./components/layout/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import CreateEmployee from './components/dashboard/CreateEmployee';
+import ShowEmployeeList from './components/dashboard/ShowEmployeeList';
+import ShowEmployeeDetails from './components/dashboard/ShowEmployeeDetails';
+import UpdateEmployeeInfo from './components/dashboard/UpdateEmployeeInfo';
+
+import "./App.css";
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div>
-          <Route exact path='/' component={ShowEmployeeList} />
-          <Route path='/create-employee' component={CreateEmployee} />
-          <Route path='/edit-employee/:id' component={UpdateEmployeeInfo} />
-          <Route path='/show-employee/:id' component={ShowEmployeeDetails} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+            <Switch>
+                <PrivateRoute exact path='/dashboard' component={ShowEmployeeList} />
+                <PrivateRoute path='/create-employee' component={CreateEmployee} />
+                <PrivateRoute path='/edit-employee/:id' component={UpdateEmployeeInfo} />
+                <PrivateRoute path='/show-employee/:id' component={ShowEmployeeDetails} />
+
+            </Switch>
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
-
 export default App;
